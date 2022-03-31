65bb435e0b67dd46ed669cb34d50c415c8648732
James Gardner <james.gardner1421@gmail.com>
2022-03-31 16:50:58 +0100

    Update

--- src/SlurmSubmission.jl
+++ src/SlurmSubmission.jl
@@ -1,6 +1,9 @@
 module SlurmSubmission
 
+using Term
+
 struct ClusterInfo
+    name::String
     ntasks_per_node::Int
     mem_per_cpu::Int
     account::String
@@ -9,24 +12,20 @@ end
 
 get_default_account() = read(`sacctmgr show user $USER format=DefaultAccount -nP`, String)
 
-const SulisCluster = ClusterInfo(128, 3850, "su007-rjm", "compute")
-const AvonCluster = ClusterInfo(48, 3700, "chemistryrjm", "compute")
-const OracCluster = ClusterInfo(28, 4571, "chemistryrjm", "cnode")
-const ArcherCluster = ClusterInfo(128, 0, "E635", "standard")
+const SulisCluster = ClusterInfo("sulis", 128, 3850, "su007-rjm", "compute")
+const AvonCluster = ClusterInfo("avon", 48, 3700, "chemistryrjm", "compute")
+const OracCluster = ClusterInfo("orac", 28, 4571, "chemistryrjm", "cnode")
+const ArcherCluster = ClusterInfo("archer", 128, 0, "E635", "standard")
 
 function ClusterInfo() 
     machine = read(`hostname`, String)
     if occursin("sulis", machine)
-        @info "Cluster identified as Sulis"
         return SulisCluster
     elseif occursin("avon", machine)
-        @info "Cluster identified as Avon"
         return AvonCluster
     elseif occursin("orac", machine)
-        @info "Cluster identified as Orac"
         return OracCluster
     elseif occursin("ln", machine)
-        @info "Cluster identified as Archer"
         return ArcherCluster
     else
         throw(error("Cluster not recognised."))
@@ -34,32 +33,55 @@ function ClusterInfo()
 end
 
 struct Options
+    cluster::ClusterInfo
     script_name::String
     sbatch_options::Vector{String}
     julia_script::String
 end
 
-function Options(julia_script; kwargs...)
-    sbatch_options = get_sbatch_options(;kwargs...)
-    Options("submit.sh", sbatch_options, julia_script)
+function Base.show(io::IO, options::Options)
+    sbatchstring = join(options.sbatch_options, "\n[green]")
+
+    sbatch_panel = Panel(
+        "[green]"*sbatchstring;
+        title="#SBATCH",
+        style="bold cyan",
+        fit=true,
+    )
+    options_panel = Panel(
+        Spacer(1,1),
+        RenderableText("[bold cyan]Cluster: [green italic]`$(options.cluster.name)`"),
+        RenderableText("[bold cyan]Script name: [green italic]`$(options.julia_script)`"),
+        Spacer(1,1),
+        sbatch_panel,
+        title="Submission options",
+        style="gold1 bold",
+        fit=true
+    )
+    print(io,
+        options_panel
+    )
 end
 
-function get_sbatch_options(;time::String, nodes=nothing,
+function Options(julia_script; kwargs...)
+    cluster = ClusterInfo()
+    sbatch_options = get_sbatch_options(cluster; kwargs...)
+    options = Options(cluster, "submit.sh", sbatch_options, julia_script)
+    print(options)
+    return options
+end
+
+function get_sbatch_options(cluster;time::String, nodes=nothing,
     partition=nothing, account=nothing, ntasks_per_node=nothing,
     total_tasks=nothing, job_name="sbatch",
 )
-
-    cluster = ClusterInfo()
-
     (partition === nothing) && (partition = cluster.partition)
     (account === nothing) && (account = cluster.account)
     (ntasks_per_node === nothing) && (ntasks_per_node = cluster.ntasks_per_node)
 
     if !(total_tasks === nothing)
-        @info "Using `total_tasks` to determine number of nodes and tasks per node."
         nodes = (total_tasks-1) ÷ cluster.ntasks_per_node + 1
         ntasks_per_node = Int(total_tasks / nodes)
-        @info "Parameters" total_tasks nodes ntasks_per_node
     elseif (nodes === nothing)
         throw(error("You must specify either `nodes` or `total_tasks`."))
     end
@@ -90,6 +112,7 @@ function write_script(options::Options, parameter_file)
             println(io, "#SBATCH $line")
         end
         println(io, "export JULIA_DEPOT_PATH=\"$(DEPOT_PATH[1])\"")
+        println(io, "export OMP_NUM_THREADS=1")
         julia_path = joinpath(Sys.BINDIR, "julia")
         println(io, join([julia_path, options.julia_script, parameter_file], " "))
     end
